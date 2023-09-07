612bcb4fd1927d73619e20af9708f1f1cc60834a
Jose Simancas <josesimancas51@gmail.com>
2023-09-07 12:42:04 -0500

    adding chakra ui

---
+++ src/components/darkModeSwitch.tsx
@@ -0,0 +1,30 @@
+'use client'
+import { 
+    FormControl, 
+    FormLabel, 
+    Switch, 
+    useColorMode 
+} from "@chakra-ui/react";
+
+export default function DarkModeSwitch() {
+    const { toggleColorMode } = useColorMode()
+    return(
+        <FormControl 
+            display='flex' 
+            alignItems='center'
+            p={4}
+        >
+            <FormLabel 
+                htmlFor='dark-mode-switch' 
+                mb='0'
+            >
+                Enable dark mode?
+            </FormLabel>
+            <Switch 
+                size={{ base: 'md',  lg: 'lg' }}
+                onChange={() => toggleColorMode()} 
+                id='dark-mode-switch' 
+            />
+        </FormControl>
+    )
+}
